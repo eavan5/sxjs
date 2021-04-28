cbb5d3e89fbbfb1374c43a16ed360f2b7a69e6da
wumao <webwwy@gmail.com>
2021-04-28 18:21:08 +0800

    基础版

---
+++ 12.promise基础版.js
@@ -0,0 +1,42 @@
+class MyPromise {
+  constructor(executor) {
+    this.state = 'PENDING'
+    this.onFulfilledCB = []
+    this.onRejectedCB = []
+    this.value = ''
+    this.reason = ''
+    executor(resolve.bind(this), reject.bind(this))
+    function resolve(value) {
+      this.state = 'FULFILLED'
+      this.value = value
+      this.onFulfilledCB.forEach(fn => fn())
+    }
+    function reject(reason) {
+      this.state = 'FULFILLED'
+      this.reason = reason
+      this.onRejectedCB.forEach(fn => fn())
+    }
+  }
+  then(onFulfilled, onRejected) {
+    if (this.state === 'FULFILLED') {
+      onFulfilled(this.value)
+    }
+    if (this.state === 'REJECTED') {
+      onRejected(this.reason)
+    }
+    if (this.state === 'PENDING') {
+      this.onFulfilledCB.push(() => onFulfilled(this.value))
+      this.onRejectedCB.push(() => onRejected(this.reason))
+    }
+  }
+}
+
+const a = new MyPromise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve(666)
+  }, 2000);
+})
+
+a.then(res => {
+  console.log(res);
+})
