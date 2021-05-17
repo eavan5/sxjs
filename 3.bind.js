8acd452879d82659e5159df6c3470c98445c9287
wumao <webwwy@gmail.com>
2021-05-18 01:23:26 +0800

    bind

--- 3.bind.js
+++ 3.bind.js
@@ -2,7 +2,7 @@ Function.prototype.myBind = function (target, ...args) {
   const thisFn = this
   let returnFn = function (...args2) {
     const isNew = this instanceof returnFn //判断是否被new
-    return thisFn.apply(isNew ? this : target, { ...args, ...args2 })
+    return thisFn.call(isNew ? this : target, ...args, ...args2)
   }
   returnFn.prototype = Object.create(thisFn.prototype); //还原老函数原型
   return returnFn
