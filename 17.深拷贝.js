604803c2eb326ab46738bd7e12fe02d56637f280
wumao <webwwy@gmail.com>
2021-05-23 22:46:31 +0800

    优化深拷贝

--- 17.深拷贝.js
+++ 17.深拷贝.js
@@ -1,7 +1,7 @@
 function deepClone(target, map = new WeakMap()) {
   if (typeof target === 'object' && target !== null) {
-    const resObj = Array.isArray(target) ? [] : {}
     if (map.has(target)) return map.get(target)
+    const resObj = Array.isArray(target) ? [] : {}
     map.set(target, resObj)
     for (const key in target) {
       resObj[key] = deepClone(target[key], map);
@@ -12,5 +12,7 @@ function deepClone(target, map = new WeakMap()) {
   }
 }
 
-const a = deepClone({ a: 1, b: { a: 1 } })
-console.log(a);
+var a = { a: 1, b: { a } }
+console.log(deepClone(a));
+
+
